168eb50f6f954e103109db5d67aa813edfdf9ce8
nbzuong <dnb.020703@gmail.com>
2022-12-30 19:03:38 +0700

    updates

--- generator.py
+++ generator.py
@@ -5,33 +5,33 @@ import string
 import puzzles
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate a random permutation of balls in tubes")
-    parser.add_argument("colours", help="number of colours", type=int)
-    parser.add_argument("height", help="height of tubes", type=int)
+    parser = argparse.ArgumentParser(description="Generate a random permutation of waters in bottles")
+    parser.add_argument("colors", help="number of colors", type=int)
+    parser.add_argument("height", help="height of bottles", type=int)
     parser.add_argument("json", help="output filename (will be written in JSON format)")
     args = parser.parse_args()
-    if(args.colours < 2 or args.colours > 52):
-        exit("Colours must be between 2-52")
+    if(args.colors < 2 or args.colors > 52):
+        exit("Colors must be between 2-52")
     if(args.height < 2):
         exit("Height must be 2 or more")
 
     l = []
-    for i in range(args.colours):
-        colour = string.ascii_letters[i]
-        cs = [colour]*args.height
+    for i in range(args.colors):
+        color = string.ascii_letters[i]
+        cs = [color]*args.height
         l += cs
     random.shuffle(l)
 
-    grid = []
+    puzzle = []
     pos = 0
-    for i in range(args.colours):
-        grid.append(l[pos:pos+args.height])
+    for i in range(args.colors):
+        puzzle.append(l[pos:pos+args.height])
         pos += args.height
-    grid.append([])
-    grid.append([])
+    puzzle.append([])
+    puzzle.append([])
 
     obj = dict()
-    obj["tubes"] = grid
+    obj["bottles"] = puzzle
 
     with open(f'puzzles/' +args.json, 'w') as json_file:
         json.dump(obj, json_file, indent=4)
