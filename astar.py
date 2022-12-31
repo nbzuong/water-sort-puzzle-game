c6a5489572ebddaa7d3c2a3b88accc2fed910bfb
nbzuong <dnb.020703@gmail.com>
2022-12-31 11:42:33 +0700

    Update astar.py

--- astar.py
+++ astar.py
@@ -6,10 +6,13 @@ from queue import PriorityQueue
 
 
 def solvePuzzle(puzzle, bottleHeight=None, visitedPositions=set(), answer=[]):
-    if bottleHeight is None:
+    if bottleHeight is None: 
         bottleHeight = max(len(t) for t in puzzle)
+    # If no bottleHeight is given, it sets the bottleHeight to the maximum length of the puzzle.
     visitedPositions.add(someConditions.puzzleToCanonicalString(puzzle))
+    # This adds the puzzle to the set of visited positions, in canonical string form.
     priorityQueue = PriorityQueue()
+    # This line creates a priority queue, which is used to store puzzle configurations.
     for i in range(len(puzzle)):
         bottle = puzzle[i]
         for j in range(len(puzzle)):
@@ -25,13 +28,22 @@ def solvePuzzle(puzzle, bottleHeight=None, visitedPositions=set(), answer=[]):
                 if(someConditions.puzzleToCanonicalString(bottle2) not in visitedPositions):
                     priorityQueue.put((getHeuristic(bottle2, bottleHeight), bottle2))
                     visitedPositions.add(someConditions.puzzleToCanonicalString(bottle2))
+    # This code loops through each bottle in the puzzle and comparing it to every other bottle in the puzzle. 
+    # If the move is valid, it creates a deep copy of the puzzle and appends the bottle it was comparing to the bottle it was looping through. 
+    # It then checks if the puzzle is solved. 
+    # If the puzzle is not solved, it adds it to a priority queue and adds it to the list of visited positions.
     while not priorityQueue.empty():
         currentPuzzle = priorityQueue.get()[1]
         solved = solvePuzzle(currentPuzzle, bottleHeight, visitedPositions, answer)
         if solved:
             answer.append(someConditions.printPuzzleToString(currentPuzzle))
             return True
+    # The while loop checks if the priority queue is empty. 
+    # If it is not empty, it gets the current puzzle from the priority queue and calls the solvePuzzle() function on it. 
+    # If the puzzle is solved, it adds the puzzle to the answer list and returns true. 
+    # If the puzzle is not solved, it continues the loop until the priority queue is empty.
     return False
+    # This line returns False if the puzzle is not solved.
 
 def getHeuristic(puzzle, bottleHeight):
     # heuristic is the number of waters that are not in the correct bottle
